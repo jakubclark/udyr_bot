dda6fadb8057469de748ea5609e41cc377b058fd
Kuba Clark <jakub.clark@protonmail.com>
2019-02-08 02:34:28 +0100

    Code refactoring

--- udyr_bot/client.py
+++ udyr_bot/client.py
@@ -1,77 +1,55 @@
-import json
 from random import randrange
 
 import discord
 
-help_text = '''
-Udyr-Bot Commands:
-        !quote  - Random Udyr quote
-        !help   - This help message
-        !github - Get the GitHub address of the project'''
-
-with open('udyr_quotes.json') as f:
-    data = json.load(f)
-quotes = data['quotes']
+from .constants import GITHUB_PROJECT_URL, HELP_TEXT, QUOTES, QUOTES_LEN
 
 
 def get_random_quote():
-    return quotes[randrange(len(quotes))]
+    return f'🔥{QUOTES[randrange(QUOTES_LEN)]}🔥'
 
 
 def get_help_text():
-    return help_text
+    return HELP_TEXT
 
 
-def github_link():
-    return 'https://github.com/jakubclark/udyr_bot'
+def get_github_url():
+    return GITHUB_PROJECT_URL
 
 
 commands = {
     '!quote': get_random_quote,
     '!help': get_help_text,
-    '!github': github_link,
-}
-
-client: discord.Client = discord.Client()
-
-
-@client.event
-async def on_ready():
-    print('Logged in as', end=' ')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
-
-@client.event
-async def on_message(message):
-    content = message.content.lower()
-
-    if not content.startswith('!') or message.author == client.user:
-        return
-
-    if content.startswith('!quote'):
-        func = commands['!quote']
-
-    elif content.startswith('!help'):
-        func = commands['!help']
-
-    elif content.startswith('!github'):
-        func = commands['!github']
-
-    else:
-        func = commands['!help']
-
-    res = func()
-    await client.send_message(message.channel, res)
-
-
-commands = {
-    '!quote': get_random_quote,
-    '!github': github_link,
-    '!help': get_help_text
+    '!github': get_github_url
 }
 
 
 def create_client():
+    client: discord.Client = discord.Client()
+
+    @client.event
+    async def on_ready():
+        print('Logged in as', end=' ')
+        print(client.user.name)
+        print(client.user.id)
+        print('------')
+
+    @client.event
+    async def on_message(message):
+        content = message.content.lower()
+
+        if not content.startswith('!') or len(content) < 2 or message.author == client.user:
+            return
+
+        first_word = content.split()[0]
+        try:
+            func = commands[first_word]
+        except KeyError:
+            func = commands['!help']
+
+        res = func()
+
+        await client.send_typing(message.channel)
+        await client.send_message(message.channel, res)
+
     return client
--- udyr_bot/constants.py
+++ udyr_bot/constants.py
@@ -1,3 +1,16 @@
+import json
 import os
 
+with open('udyr_quotes.json') as f:
+    data = json.load(f)
+
 DISCORD_BOT_TOKEN = os.environ['DISCORD_BOT_TOKEN']
+
+HELP_TEXT = '''
+Udyr-Bot Commands:
+        !quote  - Random Udyr quote
+        !help   - This help message
+        !github - Get the GitHub address of the project'''
+QUOTES = data['quotes']
+QUOTES_LEN = len(QUOTES)
+GITHUB_PROJECT_URL = 'https://github.com/jakubclark/udyr_bot'
