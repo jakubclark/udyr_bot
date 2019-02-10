acbbfa53e0ad17e7fba9f2002acd580ca984c77c
Kuba Clark <jakub.clark@protonmail.com>
2019-02-11 00:46:05 +0100

    Don't log to file

--- udyr_bot/__main__.py
+++ udyr_bot/__main__.py
@@ -7,9 +7,7 @@ from .constants import DISCORD_BOT_TOKEN
 def main():
     basicConfig(level=INFO,
                 format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                datefmt='%d-%m %H:%M:%S',
-                filename='udyr_bot.log',
-                filemode='w')
+                datefmt='%d-%m %H:%M:%S')
     client.run(DISCORD_BOT_TOKEN)
 
 
