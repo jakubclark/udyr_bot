26f63bf5bd81f2aa1f94325dfc2852d57290a97d
Kuba Clark <jakub.clark@protonmail.com>
2019-02-11 00:19:28 +0100

    Create Client class

--- udyr_bot/__main__.py
+++ udyr_bot/__main__.py
@@ -1,4 +1,4 @@
-from logging import basicConfig, INFO
+from logging import INFO, basicConfig
 
 from .client import client
 from .constants import DISCORD_BOT_TOKEN
@@ -8,8 +8,7 @@ def main():
     basicConfig(level=INFO,
                 format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                 datefmt='%d-%m %H:%M:%S',
-                filename='udyr_bot.log',
-                filemode='w')
+                filename='udyr_bot.log')
     client.run(DISCORD_BOT_TOKEN)
 
 
--- udyr_bot/client.py
+++ udyr_bot/client.py
@@ -2,47 +2,67 @@ from logging import getLogger
 
 import discord
 
-from .commands import handle_message, handle_member_join, handle_reaction_add, handle_server_join
+from .commands import commands
+from .constants import BOT_NAME
 
 log = getLogger(__name__)
 
-client: discord.Client = discord.Client()
+
+class Client(discord.Client):
+
+    async def on_ready(self):
+        log.info('Logged in as')
+        log.info(self.user.name)
+        log.info(self.user.id)
+        log.info('------')
+
+    async def on_resumed(self):
+        log.info('Bot has been resumed')
+
+    async def on_message(self, message: discord.Message):
+        try:
+            member: discord.Member = message.author
+            server = member.server
+            content = message.content
+            first_word = content.split()[0]
+
+            log.info(
+                f'Server: {server} Member: {member} Message: {message.content}')
+            if not first_word.startswith('!'):
+                return
+            try:
+                func = commands[first_word]
+            except KeyError:
+                func = commands['!help']
+
+            res = func()
+
+            await self.send_typing(message.channel)
+            await self.send_message(message.channel, res)
+        except Exception as e:
+            print(e)
+
+    async def on_reaction_add(self, reaction: discord.Reaction, member: discord.User or discord.Member):
+        log.info(f'{reaction} added by {member} to {reaction.message.content}')
+
+    async def on_member_join(self, member: discord.Member):
+        log.info(f'{member} has joined {member.server}')
+        await self.add_animal_role(member)
+
+    async def on_server_join(self, server: discord.Server):
+        log.info(f'{BOT_NAME} joined {server.name}')
+
+    async def on_error(self, event, *args, **kwargs):
+        log.info(f'Unhandled error for event: {event}')
+
+    async def add_animal_role(self, member: discord.Member):
+        for role in member.server.roles[1:]:
+            if role.name == 'Animals':
+                log.info(f'Adding the `Animals` role for {member}')
+                await self.add_roles(member, role)
+                break
+        else:
+            log.info('Did not find the `Animals` role!')
 
 
-@client.event
-async def on_ready():
-    log.info('Logged in as', end=' ')
-    log.info(client.user.name)
-    log.info(client.user.id)
-    log.info('------')
-
-
-@client.event
-async def on_resumed():
-    log.info('Bot has been resumed')
-
-
-@client.event
-async def on_message(message: discord.Message):
-    if message.channel.type == discord.ChannelType.text:
-        await handle_message(client, message)
-
-
-@client.event
-async def on_reaction_add(reaction: discord.Reaction, member: discord.User or discord.Member):
-    await handle_reaction_add(client, reaction, member)
-
-
-@client.event
-async def on_member_join(member: discord.Member):
-    await handle_member_join(client, member)
-
-
-@client.event
-async def on_server_join(server: discord.Server):
-    await handle_server_join(client, server)
-
-
-@client.event
-async def on_error(event, *args, **kwargs):
-    log.info(f'Unhandled error for event: {event}')
+client = Client()
